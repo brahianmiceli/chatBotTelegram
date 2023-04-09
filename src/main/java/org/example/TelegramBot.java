f0aecd8a28e2b8f73ba9e133058c7663481c0bb5
Jean Miceli <92497107+jeanbmiceli@users.noreply.github.com>
2023-04-08 21:55:23 -0300

    edit

--- src/main/java/org/example/TelegramBot.java
+++ src/main/java/org/example/TelegramBot.java
@@ -10,13 +10,13 @@ public class TelegramBot extends TelegramLongPollingBot {
     @Override
     public String getBotUsername() {
         //username bot - Example :"@ChatBot"
-        return "";
+        return "here username";
     }
 
     @Override
     public String getBotToken() {
         //token bot - Example "9999:abcdefghijklmnqzzz3a33"
-        return "";
+        return "here token";
     }
 
     //we handle the received update and capture the text and id of the conversation
